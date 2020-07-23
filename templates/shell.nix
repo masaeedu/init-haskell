2772162a2a4932dbef74078bf414dba2cb0c5b63
Asad Saeeduddin <masaeedu@gmail.com>
2020-07-23 09:06:51 -0400

    Add ghcid and hpack to shell environment

--- templates/shell.nix
+++ templates/shell.nix
@@ -1,10 +1,15 @@
 let
   sources = import ./nix/sources.nix;
-  compilerVersion = "{{ghc}}";
+  compilerVersion = "ghc883";
   ghcide = (import sources.ghcide-nix {})."ghcide-${compilerVersion}";
-  hspkgs = import ./.;
+  hnix = import sources.iohk-hnix {};
+  pkgs = (import hnix.sources.nixpkgs) hnix.nixpkgsArgs;
 in
-hspkgs.shellFor {
+(import ./.).shellFor {
   withHoogle = true;
-  buildInputs = [ ghcide ];
+  buildInputs = [
+    ghcide
+    (pkgs.haskell-nix.tool compilerVersion "hpack" "0.34.2")
+    (pkgs.haskell-nix.tool compilerVersion "ghcid" "0.8.7")
+  ];
 }
