88d3706bd8c4179e2e6b71985f695ce2b6d53adc
Asad Saeeduddin <masaeedu@gmail.com>
2020-09-18 14:45:51 -0400

    Fix missing template reference, fix cabal version

--- templates/shell.nix
+++ templates/shell.nix
@@ -1,6 +1,6 @@
 let
   sources = import ./nix/sources.nix;
-  compilerVersion = "ghc883";
+  compilerVersion = "{{ghc}}";
   ghcide = (import sources.ghcide-nix {})."ghcide-${compilerVersion}";
   hnix = import sources.iohk-hnix {};
   pkgs = (import hnix.sources.nixpkgs) hnix.nixpkgsArgs;
@@ -9,7 +9,8 @@ in
   withHoogle = true;
   buildInputs = [
     ghcide
-    (pkgs.haskell-nix.tool compilerVersion "hpack" { index-state = "{{snapshot}}"; version = "0.34.2"; })
-    (pkgs.haskell-nix.tool compilerVersion "ghcid" { index-state = "{{snapshot}}"; version = "0.8.7"; })
+    (pkgs.haskell-nix.tool compilerVersion "hpack"         { index-state = "{{snapshot}}"; version = "0.34.2"; })
+    (pkgs.haskell-nix.tool compilerVersion "cabal-install" { index-state = "{{snapshot}}"; version = "3.2.0.0"; })
+    (pkgs.haskell-nix.tool compilerVersion "ghcid"         { index-state = "{{snapshot}}"; version = "0.8.7";  })
   ];
 }
