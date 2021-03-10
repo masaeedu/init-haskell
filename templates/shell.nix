eb9afef1b742a9eeea961d6ca8913ffbe63c1706
Asad Saeeduddin <masaeedu@gmail.com>
2021-03-10 03:16:00 -0500

    Tweak tooling versions so we don't have to build a whole Cabal
    https://github.com/input-output-hk/haskell.nix/pull/1058#issuecomment-795032020

--- templates/shell.nix
+++ templates/shell.nix
@@ -10,8 +10,8 @@ in
   withHoogle = true;
   buildInputs = [
     hls
-    (pkgs.haskell-nix.tool compilerVersion "hpack"         { index-state = snapshot; version = "0.34.2"; })
-    (pkgs.haskell-nix.tool compilerVersion "cabal-install" { index-state = snapshot; version = "3.2.0.0"; })
+    (pkgs.haskell-nix.tool compilerVersion "hpack"         { index-state = snapshot; version = "0.34.4"; })
+    (pkgs.haskell-nix.tool compilerVersion "cabal-install" { index-state = snapshot; version = "3.4.0.0"; })
     (pkgs.haskell-nix.tool compilerVersion "ghcid"         { index-state = snapshot; version = "0.8.7";  })
   ];
 }
