3ced6078b21f4946040a565c9347722cbcd25cd0
Asad Saeeduddin <masaeedu@gmail.com>
2021-04-04 14:00:55 -0400

    Use shajra/nix-haskell-hls

--- templates/shell.nix
+++ templates/shell.nix
@@ -3,13 +3,14 @@ let
   compilerVersion = import ./compiler.nix;
   hnix = import sources.iohk-hnix {};
   pkgs = (import hnix.sources.nixpkgs) hnix.nixpkgsArgs;
-  hls = import sources.all-hls { inherit pkgs; version = "1.0.0"; ghc = "8.10.4"; }; # TODO: generate this string from the ghc template variable
+  hls = import sources.nix-haskell-hls { ghcVersion = compilerVersion; hlsUnstable = true; };
   snapshot = import ./snapshot.nix;
 in
 (import ./.).shellFor {
   withHoogle = true;
   buildInputs = [
-    hls
+    hls.hls-renamed
+    hls.hls-wrapper
     (pkgs.haskell-nix.tool compilerVersion "hpack"         { index-state = snapshot; version = "0.34.4"; })
     (pkgs.haskell-nix.tool compilerVersion "cabal-install" { index-state = snapshot; version = "3.4.0.0"; })
     (pkgs.haskell-nix.tool compilerVersion "ghcid"         { index-state = snapshot; version = "0.8.7";  })
