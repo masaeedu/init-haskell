2afa3a3e569511963c4e773456e70f8876a6e220
Asad Saeeduddin <masaeedu@gmail.com>
2021-10-21 11:31:14 -0300

    Update stuff

--- templates/default.nix
+++ templates/default.nix
@@ -2,9 +2,20 @@ let
   sources = import ./nix/sources.nix;
   compilerVersion = import ./compiler.nix;
   hnix = import sources.iohk-hnix {};
-  pkgs = (import hnix.sources.nixpkgs) hnix.nixpkgsArgs;
+  pkgs = (import hnix.sources.nixpkgs-unstable) hnix.nixpkgsArgs;
+  snapshot = import ./snapshot.nix;
 in
-pkgs.haskell-nix.cabalProject {
-  src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
-  compiler-nix-name = compilerVersion;
+rec {
+  inherit hnix pkgs;
+
+  cabal-install =
+    pkgs.haskell-nix.tool compilerVersion "cabal-install" { index-state = snapshot; version = "3.4.0.0"; };
+
+  project =
+    pkgs.haskell-nix.cabalProject {
+      src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; name = "{{projectname}}"; };
+      compiler-nix-name = compilerVersion;
+      inherit cabal-install;
+    };
 }
+
--- templates/shell.nix
+++ templates/shell.nix
@@ -1,18 +1,19 @@
 let
   sources = import ./nix/sources.nix;
   compilerVersion = import ./compiler.nix;
-  hnix = import sources.iohk-hnix {};
-  pkgs = (import hnix.sources.nixpkgs) hnix.nixpkgsArgs;
-  hls = import sources.nix-haskell-hls { ghcVersion = compilerVersion; hlsUnstable = true; };
+  nix-haskell-hls = import sources.nix-haskell-hls;
   snapshot = import ./snapshot.nix;
 in
-(import ./.).shellFor {
-  withHoogle = true;
+with import ./.;
+let hls = nix-haskell-hls { ghcVersion = (project.ghcWithPackages (_: [])).version; hlsUnstable = true; };
+in
+project.shellFor {
+  withHoogle = false;
   buildInputs = [
     hls.hls-renamed
     hls.hls-wrapper
+    cabal-install
     (pkgs.haskell-nix.tool compilerVersion "hpack"         { index-state = snapshot; version = "0.34.4"; })
-    (pkgs.haskell-nix.tool compilerVersion "cabal-install" { index-state = snapshot; version = "3.4.0.0"; })
     (pkgs.haskell-nix.tool compilerVersion "ghcid"         { index-state = snapshot; version = "0.8.7";  })
   ];
 }
